4ccca1e4753fc5e3d1aeea0be8f0d9fd4576000e
Chowdhury tanvir ahmed <33882712+tanvirGreat@users.noreply.github.com>
2018-10-24 20:31:49 +0100

    Add files via upload

---
+++ LoremIpsum.js
@@ -0,0 +1,24 @@
+ function HideShow1() {
+    var x = document.getElementById('rollover1');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+ function HideShow2() {
+    var x = document.getElementById('rollover2');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+ function HideShow3() {
+    var x = document.getElementById('rollover3');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
